bfed187644c9f63b1490230a39767e3c847ffc52
tusharnika <99320497+tusharnika@users.noreply.github.com>
2023-05-31 22:30:47 +0530

    Add files via upload

---
+++ jqu.js
@@ -0,0 +1,10 @@
+$(document).ready(function()){
+    //variab;le
+    $game= $('.game');
+    $character=$('.character');
+    //keypress wevent
+
+    $(document).on('keypress',function(e){
+        console.log(e.which);
+    })
+}
